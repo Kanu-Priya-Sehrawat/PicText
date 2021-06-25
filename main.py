70c925820a2a8d402e8581650174f28367aad087
Kanu-Priya-Sehrawat <priyakanu1116@gmail.com>
2021-06-25 12:56:29 +0530

    Updated

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from PIL import ImageGrab
 import time
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+# Write the image name
 imgName = "TextDocument.png"
 img = cv2.imread(imgName)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -13,9 +15,8 @@ string = ""
 hImg, wImg,_ = img.shape
 boxes = pytesseract.image_to_boxes(img)
 for b in boxes.splitlines():
-    #print(b[0],end="")
-
-    #string = string + b
+    print(b[0],end="")
+    string = string + b[0]
     b = b.split(' ')
     #print(b)
     
@@ -27,13 +28,9 @@ for b in boxes.splitlines():
     #This is also for putting text over the box
     #cv2.putText(img,b[0],(x,hImg- y+25),cv2.FONT_HERSHEY_SIMPLEX,1,(50,50,255),2)
 
-
-# cv2.imshow('img', img)
-# cv2.waitKey(0)
-
-string = pytesseract.image_to_string(img)
-print(string)
+# This code is writing each letter of the png file in the string.txt file
 with open("string.txt",'w') as f:
-    f.write(string)
+        f.write(string)
+
 cv2.imshow('img', img)
 cv2.waitKey(0)
